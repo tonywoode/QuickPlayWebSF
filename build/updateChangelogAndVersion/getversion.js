033ccb08fdc65db2afda8c2a68df2d44fa82ed35
tony woode <tonywoodecode@gmail.com>
2015-06-14 22:46:17 +0100

    read auto-generated version list (no longer manual)

--- build/updateChangelogAndVersion/getversion.js
+++ build/updateChangelogAndVersion/getversion.js
@@ -1,8 +1,8 @@
 var fs			= require('fs');
 
 module.exports = function() {
-	var filename  = '../../../QuickPlayFrontend/Version.txt';
-	var pattern = new RegExp(/^THISVERSION=v(.*)/);
+	var filename  = '../../../Release/Version.txt';
+	var pattern = new RegExp(/^refs\/tags\/v(.*)/);
 	var text = fs.readFileSync(filename, 'ascii');
 	var match = text.match(pattern);
 	var version = (match[1]);
