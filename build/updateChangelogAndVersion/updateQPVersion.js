174196e8ff6965d4dd0983455c4307758f65b088
tony woode <tonywoodecode@gmail.com>
2015-09-02 08:16:33 +0100

    manual detail-entry (scaffold) replaced with auto imp

--- build/updateChangelogAndVersion/updateQPVersion.js
+++ build/updateChangelogAndVersion/updateQPVersion.js
@@ -1,25 +1,24 @@
 /**
  * Reads the local copy of the qp database and updates QPVer
  */
-var 		qpVer				= process.env.version, //quickplay version number we're releasing
-				qpZip				= process.env.localZipFile,
-				fs					= require("fs"),
-				stats				= fs.statSync(qpZip),
-				zipSizeInBytes = stats["size"],
-				zipSizeInMB = zipSizeInBytes / 1000000.0;
-
-				
-console.log("let's release:");
-console.log(qpVer);
-console.log("which is this size:");
-console.log(zipSizeInMB);
-process.exit();
-
-var prompt      = require('./userInput'),
-    getVersion  = require('./getVersion'),
-    version_name = getVersion(), //to force return value not function itself
-    connection  = require('./dbConnect');
-
+var 		fs					= require("fs"),
+				connection  = require('./dbConnect'),
+			 	version_name = process.env.version, //quickplay version number we're releasing
+			qpZip       = process.env.localZipFile,
+			stats       = fs.statSync(qpZip),
+			zipSizeInBytes = stats["size"],
+			filesize = Math.floor(zipSizeInBytes / 1000000) + "MB";
+			downloadURL = process.env.sfFileURL; 
+/*
+function updateQPVerManually() { 
+	var prompt      = require('./userInput'),
+			getVersion  = require('./getVersion'),
+			version_name = getVersion(), //to force return value not function itself
+			downloadURl = result.downloadURL,
+			filesize		= result.filesize;
+			
+}
+*/
 
 //the callback we pass to connect runs after we connect, or fail to
 connection.connect(function(err) {
@@ -29,23 +28,22 @@ connection.connect(function(err) {
     }
 
     console.log('connected as id ' + connection.threadId);
-
+});
 /*
 the anonymous function we just defined has the same signature as showTable,
  so we can just pass the reference to show table rather than defining an anonymous function
  you could just go prompt(showTable); you only need anonymous function if you're going to change 
  the signature or if there is no named function to invoke
  */
-    console.log('Enter the details for Version ' + version_name);//force string return value not function code
-prompt(function(result){ 
+  //  console.log('Enter the details for Version ' + version_name);//force string return value not function code
+//prompt(function(result){ 
     //async-101: we can't call insertRow serially after show table, or it'll run both at the same time
-    showTable(result, function(){
-            insertRow(version_name, result.downloadURL, result.filesize, result)
+		showTable(function() {
+           insertRow(version_name, downloadURL, filesize)
             }); //here's why we use promises, this could become the callback pyramid of doom
-    });
-});
+//});
 
-function showTable(result, next) {
+function showTable(next) {
     console.log('EXISTING VERSION TABLE FOLLOWS:');
     connection.query('select * from version', function (error, results, fields) {
         // error will be an Error if one occurred during the query
@@ -54,12 +52,12 @@ function showTable(result, next) {
         if (results) { console.log('RESULTS=', results); }
         // fields will contain information about the returned results fields (if any)
         if (fields) { console.log('FIELDS=', fields); }
-        next(result);
+        next();
     });
 }
 
 //insert values for the next tuple - primary key version_id in the table auto-increments
-function insertRow(version_name, downloadURL, filesize, next) {
+function insertRow(version_name, downloadURL, filesize) {
     console.log('Enter the details for Version ' + version_name);
     connection.query('INSERT INTO version (version_name, download, filesize) values ("'+version_name+'","'+downloadURL+'", "'+filesize+'")',
         function (error, results, fields) {
