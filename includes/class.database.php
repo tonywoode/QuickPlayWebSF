969bbba9b3c2cd48ab05c1fcd88a52cd0658d3f4
tony woode <tonywoodecode@gmail.com>
2015-10-28 20:03:23 +0000

    now iterating - advancing per call

--- includes/class.database.php
+++ includes/class.database.php
@@ -33,57 +33,47 @@ class QPDatabase{
     $this->conn = new mysqli($this->dbhost, $this->dbusername, $this->dbpassword, $this->dbname, $this->dbport);
   }
 
-	function iimysqli_stmt_get_result($stmt){
-	    //$metadata = mysqli_stmt_result_metadata($stmt);
+	function iimysqli_stmt_get_result($stmt)
+	{
+	    $metadata = mysqli_stmt_result_metadata($stmt);
 				    $ret = new iimysqli_result;
 				    if (!$ret) return NULL;
-						  $result = array();
-						  $stmt->store_result();
-							for ( $i = 0; $i < $stmt->num_rows; $i++ ) {
-									$Metadata = $stmt->result_metadata();
-						    $ret->nCols = mysqli_num_fields($Metadata);
-									$params = array();
-									while ( $Field = $Metadata->fetch_field() ) {
-										$params[] = &$result[ $i ][ $Field->name ];
-									}
-								  call_user_func_array( array( $stmt, 'bind_result' ), $params );
-									$stmt->fetch();
-							}
-							
 
-						    $ret->stmt = $result;
+						    $ret->nCols = mysqli_num_fields($metadata);
+						    $ret->stmt = $stmt;
 
-								   // mysqli_free_result($metadata);
-
-								return $ret;
+								    mysqli_free_result($metadata);
+								    return $ret;
 	}
 
 	function iimysqli_result_fetch_array(&$result){
-		//	echo "here IS result:";
-		//	print_r($result);
-		//echo "here's &result in the fetcharray method";	
-		//var_dump($result);	
-	echo "here we are echoing statement from result_fetch_array";
-	var_dump($result->stmt);
-	echo "now get version name value";
-	echo ($result->stmt[0]["version_name"]);
-	echo "and lastly the key";
-	foreach($result->stmt[0] as $key=>$value) { echo $key; };	
-		$ret = array();
-			$code = "return mysqli_stmt_bind_result(\$result->stmt ";
-			for ($i=0; $i<$result->nCols; $i++){
+		echo "number of rows is";
+			print_r($result->stmt->num_rows);
+		echo "number of cols is";
+		print_r($this->otherresult->nCols);
+		$ohgod = array();
+		$stmt = $result->stmt;
+		$stmt->store_result();
+		$params = array();
+		for ( $i = 0; $i < $stmt->num_rows; $i++ ) {
+						$Metadata = $stmt->result_metadata();
+						while ( $Field = $Metadata->fetch_field() ) {
+								$params[] = &$ohgod[ $i ][ $Field->name ];
+						}
+				  call_user_func_array( array( $stmt, 'bind_result' ), $params );
+											//		$stmt->fetch();
+	}
+			$ret = array();
+		$code = "return mysqli_stmt_bind_result(\$result->stmt ";
+			for ($i=0; $i<$this->otherresult->nCols; $i++){
 				$ret[$i] = NULL;
-				$code .= ", \$ret['" .$i ."']";
+				$code .= ", \$ret['" .$params[$i] ."']";
 			};
 
 			$code .= ");";
-		echo "code is:";
-			print_r($code);
 			if (!eval($code)) { return NULL; };
-
 			// This should advance the "$stmt" cursor.
 			if (!mysqli_stmt_fetch($result->stmt)) { return NULL; };
-								
 			// Return the array we built.
 			return $ret;
 	}
@@ -100,22 +90,17 @@ class QPDatabase{
 		elseif ($arg1 != "" && $arg2 != "") {
 			$stmt->bind_param("ss", $arg1, $arg2); }
 		$stmt->execute(); 
-		//$stmt->store_result(); //this is about buffering the result, get more output with iimysqli arrays, but breaks mysqli ones
+		$stmt->store_result(); //this is about buffering the result, get more output with iimysqli arrays, but breaks mysqli ones
 		//$result = $stmt->get_result();	
 		//var_dump( $this->iimysqli_stmt_get_result($stmt));
 		$otherresult = $this->iimysqli_stmt_get_result($stmt);
-		//echo "here's the results object";
-		//var_dump($otherresult);
 		$this->otherresult = $otherresult; 
 		//$this->lastqueryresult = $result; 
 		//var_dump($this->Fetch_Full_array());
-		//	echo "here's global result var";
-		//var_dump($this->otherresult);	
+		//	echo "here's result";
 		//	print_r($this->otherresult);
 		//var_dump( $result);
-	  //var_dump($result->fetch_array(MYSQLI_ASSOC));//this gives assoc array only 
-		//var_dump( $this->iimysqli_result_fetch_array($this->otherresult));//this gives numeric array
-		
+		//var_dump( $this->iimysqli_result_fetch_array($this->otherresult));
 		if ( mysqli_error($this->conn) != "" ) {
       $this->lasterror = mysqli_error($this->conn);
       $this->lasterrno = mysqli_errno($this->conn);
@@ -138,15 +123,11 @@ class QPDatabase{
   }
 
   function Fetch_Array() {
-		//	echo "here's global result var in the fetch array function";
-		//var_dump($this->otherresult);	
-			
-			//var_dump( mysqli_fetch_array($this->lastqueryresult));
+		//var_dump( mysqli_fetch_array($this->lastqueryresult));
 		//return mysqli_fetch_array($this->lastqueryresult);
-		//echo "array is";
+		//echo "ultimately, array is";
 		//print_r($this->iimysqli_result_fetch_array($this->otherresult));
-		echo "here's the result var being called from fetch array";
-		var_dump( $this->iimysqli_result_fetch_array($this->otherresult));
+		return( $this->iimysqli_result_fetch_array($this->otherresult));
 	}
 
   function Fetch_Full_Array() {
