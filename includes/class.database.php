c260de3319170216256b021b91bc844aea0d5ea6
tony woode <tonywoodecode@gmail.com>
2015-10-29 13:10:51 +0000

    cleanup
    still not sure where to close statement given current structure

--- includes/class.database.php
+++ includes/class.database.php
@@ -1,29 +1,27 @@
 <?php
 
-class iimysqli_result
-{
-	    public $stmt, $nCols;
+//convenience class to give ourselves a results object to pass around like get_result would have done
+class iimysqli_result {
+	    public $stmt, $ncols;
 }    
 // This file is the MySQL database class, which wraps a lot of common MySQL functions in an object.
 // Based on a class written by Dave Frame (www.phptrix.co.uk), and modified to suit this project.
 
-class QPDatabase{
+class QPDatabase {
 
   var $dbhost, $dbport, $dbusername, $dbpassword, $dbname;
-  var $type, $bindtype, $stmt, $result, $conn, $lastqueryresult, $lasterror, $lasterrno, $lastinsertid, $otherresult;
+  var $type, $bindtype, $stmt, $result, $conn, $lastqueryresult, $lasterror, $lasterrno, $lastinsertid, $iilastqueryresult;
 
   // ---- Constructor ----
 
   function QPDatabase() {
-    // Sourceforge Settings.
-    
     $this->dbhost = "";
     $this->dbport = ;
     $this->dbusername = "";
     $this->dbpassword = "";
     $this->dbname = "";
-    $this->otherresult = "";
-    $this->lastqueryresult = "";
+    $this->iilastqueryresult = "";
+    //$this->lastqueryresult = "";//without a sqlnd driver on the server, we needed our own result object
     $this->Open();
   }
 
@@ -33,131 +31,102 @@ class QPDatabase{
     $this->conn = new mysqli($this->dbhost, $this->dbusername, $this->dbpassword, $this->dbname, $this->dbport);
   }
 
-	function iimysqli_stmt_get_result($stmt)
-	{
-	    $metadata = mysqli_stmt_result_metadata($stmt);
-				    $ret = new iimysqli_result;
-				    if (!$ret) return NULL;
-
-						    $ret->nCols = mysqli_num_fields($metadata);
-						    $ret->stmt = $stmt;
-
-								    mysqli_free_result($metadata);
-								    return $ret;
+	// ii methods based on anonymously-posted code at the mysqli_get_result_api docs
+	// however these assumed numeric array was fine for callsites. The code here uses assoc arrays exlusively
+	function iimysqli_get_result($stmt) {
+	  $metadata = $stmt->result_metadata();
+		$ret = new iimysqli_result;
+		if (!$ret) return NULL;
+		$ret->ncols = $metadata->field_count;
+		$ret->stmt = $stmt;
+		$metadata->free_result();
+		return $ret;
 	}
 
-	function iimysqli_result_fetch_array(&$result){
-		//echo "number of rows is";
-	//		print_r($result->stmt->num_rows);
-	//	echo "number of cols is";
-	//	print_r($this->otherresult->nCols);
-		$ohgod = array();
+	//this mimics mysqli_fetch_array by returning a new row each time until exhausted
+	function iimysqli_result_fetch_array(&$result) {
 		$stmt = $result->stmt;
 		$stmt->store_result();
-		$params = array();
-		$theKeys = array();
+		$resultkeys = array();
 		$thisName = "";
 		for ( $i = 0; $i < $stmt->num_rows; $i++ ) {
-						$Metadata = $stmt->result_metadata();
-						while ( $Field = $Metadata->fetch_field() ) {
-							//echo "at this point field name is";
-							//print_r($Field->name);
-							$thisName = $Field->name;
-							$theKeys[] = $thisName;
-							//var_dump($theKeys);
-							//echo "END";
-	//							$params[] = &$ohgod[ $i ][ $Field->name ];
-						}
-//				  call_user_func_array( array( $stmt, 'bind_result' ), $params );
-												//	$stmt->fetch();
-	}
-		//echo "at this point params is";
-//		var_dump($ohgod);
+				$metadata = $stmt->result_metadata();
+				while ( $field = $metadata->fetch_field() ) {
+					$thisName = $field->name;
+					$resultkeys[] = $thisName;
+				}
+		}
 			
 		$ret = array();
 		$code = "return mysqli_stmt_bind_result(\$result->stmt ";
-			for ($i=0; $i<$this->otherresult->nCols; $i++){
-				$ret[$i] = NULL;
-		//		echo "here in the loop we need this name is";
-		//		echo $thisName;
-				//		echo "FIN";
-				$theValue = $theKeys[$i];
-				$code .= ", \$ret['$theValue']";
-			};
-
+		for ($i=0; $i<$result->ncols; $i++) {
+			$ret[$i] = NULL;
+			$theValue = $resultkeys[$i];
+			$code .= ", \$ret['$theValue']";
+		};
+		
 		$code .= ");";
-		//echo "what the hell does this do";
-		//echo $code;
-			if (!eval($code)) { return NULL; };
-			// This should advance the "$stmt" cursor.
-			if (!mysqli_stmt_fetch($result->stmt)) { return NULL; };
-			// Return the array we built.
-			return $ret;
+		if (!eval($code)) { 
+			return NULL; 
+		};
+		
+		// This should advance the "$stmt" cursor.
+		if (!mysqli_stmt_fetch($result->stmt)) { 
+			return NULL; 
+		};
+
+		// Return the array we built.
+		return $ret;
 	}
 
-	function Query($querystring, $arg1, $arg2){
-		
-	//		echo "you said do this query:";
-	//		echo $querystring;
-	//		echo $arg1;
-	//		echo $arg2;
+	//quick fix in lieu of varags: I noticed we never have more than two prepared bindings
+	function Query($querystring, $arg1, $arg2) {
 		$stmt = $this->conn->prepare($querystring);
 		if ($arg1 != "" && $arg2 == "") {
-			$stmt->bind_param("s", $arg1); }
+			$stmt->bind_param("s", $arg1); 
+		}
 		elseif ($arg1 != "" && $arg2 != "") {
-			$stmt->bind_param("ss", $arg1, $arg2); }
+			$stmt->bind_param("ss", $arg1, $arg2); 
+		};
+
 		$stmt->execute(); 
-		$stmt->store_result(); //this is about buffering the result, get more output with iimysqli arrays, but breaks mysqli ones
-		//$result = $stmt->get_result();	
-		//var_dump( $this->iimysqli_stmt_get_result($stmt));
-		$otherresult = $this->iimysqli_stmt_get_result($stmt);
-		$this->otherresult = $otherresult; 
-		//$this->lastqueryresult = $result; 
-		//var_dump($this->Fetch_Full_array());
-		//	echo "here's result";
-		//	print_r($this->otherresult);
-		//var_dump( $result);
-		//var_dump( $this->iimysqli_result_fetch_array($this->otherresult));
+		$stmt->store_result(); 
+		//$result = $stmt->get_result(); //lack of sqlnd on sourceforge servers prevents us using this. Really tough luck!	
+		$iilastqueryresult = $this->iimysqli_get_result($stmt);
+		$this->iilastqueryresult = $iilastqueryresult; 
 		if ( mysqli_error($this->conn) != "" ) {
       $this->lasterror = mysqli_error($this->conn);
       $this->lasterrno = mysqli_errno($this->conn);
-		//	$stmt->close();
+			$stmt->close();
 			return 0;
     }
 		else
-			//$stmt->close();
+			//$stmt->close();	//TODO:since we can't get_result, we can't close the statment now, we need lots more info from it
 			return 1;
   }
 
-  function Num_Rows() { 
-		//return count($this->iimysqli_result_fetch_array($this->otherresult));
-		//return mysqli_num_rows($this->lastqueryresult);
-		return $this->otherresult->stmt->affected_rows;
+	function Num_Rows() {
+		return $this->iilastqueryresult->stmt->num_rows;
 	}
 
-  function Affected_Rows(){
-    return mysqli_affected_rows($this->lastqueryresult);
+  function Affected_Rows() {
+    return mysqli_affected_rows($this->lastqueryresult);//not being used
   }
 
   function Fetch_Array() {
-		//var_dump( mysqli_fetch_array($this->lastqueryresult));
-		//return mysqli_fetch_array($this->lastqueryresult);
-		//echo "ultimately, array is";
-		//print_r($this->iimysqli_result_fetch_array($this->otherresult));
-		return( $this->iimysqli_result_fetch_array($this->otherresult));
+		return $this->iimysqli_result_fetch_array($this->iilastqueryresult);
 	}
 
   function Fetch_Full_Array() {
     $allrows = array();
-    //while ( $currentrow = mysqli_fetch_array($this->lastqueryresult) ) {
-    while ( $currentrow = $this->iimysqli_result_fetch_array($this->otherresult) ) {
-            array_push($allrows, $currentrow);
+    while ( $currentrow = $this->iimysqli_result_fetch_array($this->iilastqueryresult) ) {
+       array_push($allrows, $currentrow);
     }
     return $allrows;
   }
 
-  function GetResult(){
-    return $this->lastqueryresult;
+  function GetResult() {
+    return $this->lastqueryresult;//not being used
   }
 
   function Error() {
@@ -176,31 +145,31 @@ class QPDatabase{
     mysqli_close($this->conn);
   }
   
-  function WikiPageExists($pagename){
+  function WikiPageExists($pagename) {
     $query = "SELECT p_name FROM pages WHERE p_name=(?)";
-    if ($this->Query($query, $pagename, "") == 1){
+    if ($this->Query($query, $pagename, "") == 1) {
       if ($this->Num_Rows() > 0)
         return true;
       else
         return false;
     }
-    else{
+    else {
       //the query failed return false
       return false;
     }
     
   }
   
-  function WikiGetPage($pagename){
+  function WikiGetPage($pagename) {
     $query = "SELECT * FROM pages WHERE p_name=(?)";
-    if ($this->Query($query, $pagename, "") == 1){
-      if ($this->Num_Rows() == 1){
+    if ($this->Query($query, $pagename, "") == 1) {
+      if ($this->Num_Rows() == 1) {
         return $this->Fetch_Full_Array();
       }
       else
         return false;
     }
-    else{
+    else {
       //the query failed return false
       return false;
     }
