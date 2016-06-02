b3af39d55253eb1c91c208dc44de97a37af89b0f
tony woode <tonywoodecode@gmail.com>
2016-06-02 11:49:30 +0100

    formatting js

--- build/updateChangelogAndVersion/updateChangelog.js
+++ build/updateChangelogAndVersion/updateChangelog.js
@@ -1,114 +1,104 @@
 /**
-We use the changelog and version info produced via git of the lastest quickplay changes,
- we produce an html update of those changes for the sourceforge website
-*/
-var fs					= require('fs'),
-		execFile		= require('child_process').execFile,
-		optipng			= require('pandoc-bin').path,	
-		connection  = require('./dbConnect'),
-		/*
-		In Node, __dirname is always the dir in which the currently executing script resides. 
-		In other words, you typed __dirname into one of your script files and value would be 
-		that file's directory. By contrast, . gives you the directory from which you ran the 
-		node command in your terminal window (i.e. your working directory). 
-		The exception is when you use . with require(), in which case it acts like __dirname.
-		so whilst require is rooted in this file's dir, fs appears rooted in the callers dir one level up
-		so this is rooted in the js scripts folder:
-		*/
-  	prompt  	= require('prompt'),
-   	
-		//whilst these are rooted in the build folder (parent):
-		changelogLF	= '../../../Release/changelogLF',
-		changelogHTML = '../../../Release/changelogHTML',
-		changeText = fs.readFileSync('../../../Release/changelogHTML', 'ascii'),
-	
-		//...we can do this to avoid this ambiguity, but its a little ugly
-		getVersion = require( '' + __dirname + '/getVersion'),
+  * We use the changelog and version info produced via git of the lastest quickplay changes,
+  * we produce an html update of those changes for the sourceforge website
+  */
+var fs = require('fs'),
+  execFile = require('child_process').execFile,
+  optipng	= require('pandoc-bin').path,
+  connection = require('./dbConnect'),
+  /*
+   * In Node, __dirname is always the dir in which the currently executing script resides. 
+   * In other words, you typed __dirname into one of your script files and value would be 
+   * that file's directory. By contrast, . gives you the directory from which you ran the 
+   * node command in your terminal window (i.e. your working directory). 
+   * The exception is when you use . with require(), in which case it acts like __dirname.
+   * so whilst require is rooted in this file's dir, fs appears rooted in the callers dir one level up
+   * so this is rooted in the js scripts folder:
+  */
+  prompt  	= require('prompt'),
 
-		version    = getVersion(),
-		argsToPandoc = ['-f', 'markdown', '-t', 'html', changelogLF,'-o', changelogHTML],
+  //whilst these are rooted in the build folder (parent):
+  changelogLF	= '../../../Release/changelogLF',
+  changelogHTML = '../../../Release/changelogHTML',
+  changeText = fs.readFileSync('../../../Release/changelogHTML', 'ascii'),
 
-		date 		= new Date().toISOString().slice(0, 10).replace('T', ' '),
-		
-		//my id in the qp database
-		tony		= 5; 
+  //...we can do this to avoid this ambiguity, but its a little ugly
+  getVersion = require( '' + __dirname + '/getVersion'),
 
+  version    = getVersion(),
+  argsToPandoc = ['-f', 'markdown', '-t', 'html', changelogLF,'-o', changelogHTML],
+
+  date 		= new Date().toISOString().slice(0, 10).replace('T', ' '),
+
+  //my id in the qp database
+  tony		= 5; 
 
 printWhatWeStartWith(function(callback){ 
-       yesorno(function(answer){
-            pandocIt(answer, function() {
-            	insertChangelog(date, getVersion(), changeText, tony);
-            });
-        });
-       
+  yesorno(function(answer){
+    pandocIt(answer, function() {
+      insertChangelog(date, getVersion(), changeText, tony);
+    });
+  });
 });
 
 function printWhatWeStartWith (callback) {	
-	console.log('Updating changelog for ' + version + ': Here\'s the text pulled earlier:');
-	var changelog = fs.readFileSync(changelogLF);
-	console.log('' + changelog);
-	callback();
-
+  console.log('Updating changelog for ' + version + ': Here\'s the text pulled earlier:');
+  var changelog = fs.readFileSync(changelogLF);
+  console.log('' + changelog);
+  callback();
 }
 
-function yesorno (next) { //next holds the address of the function i declared in yesorno's callsite currently on line 35
-	// dbc - assert everything is okay before the real work starts
-	if (typeof next !== 'function' && next.length !== 1)
-		throw new Error('next must be a function that takes 1 argument');
+function yesorno (next) { //next holds the address of the function i declared in yesorno's callsite currently on line 37
+  // dbc - assert everything is okay before the real work starts
+  if (typeof next !== 'function' && next.length !== 1) {
+    throw new Error('next must be a function that takes 1 argument');
+  }
+  prompt.start();
+  prompt.colors = false;
+  var property = {
+  name: 'yesno',
+  message: 'are you sure?',
+  validator: /y[es]*|n[o]?/,
+  warning: 'Must respond yes or no',
+  default: 'no'
+  };
 
-	prompt.start();
-	prompt.colors = false;
-	var property = {
-	  name: 'yesno',
-	  message: 'are you sure?',
-	  validator: /y[es]*|n[o]?/,
-	  warning: 'Must respond yes or no',
-	  default: 'no'
-	};
-
-	// Get the yes or no property
-	prompt.get(property, function (err, result) {
-	  // Log the results.
-	  console.log('  result: ' + result.yesno);
-	  decision = result.yesno;
-	  next(result.yesno);
-	});
-
+  // Get the yes or no property
+  prompt.get(property, function (err, result) {
+    // Log the results.
+    console.log('  result: ' + result.yesno);
+    decision = result.yesno;
+    next(result.yesno);
+  });
 }
 
 function pandocIt (doItOrNot, next) {	
-	if ( doItOrNot==='y' ) {
-		execFile(optipng, argsToPandoc, function (err, stdout, stderr) {
-		    console.log(stdout);
-		    if ( err ) { console.log(err); } //else prints null
-		    console.log(stderr);
-		    if ( !err ) { next(); } //heres the callback....
-		});
-	
-	}
-
+  if ( doItOrNot==='y' ) {
+    execFile(optipng, argsToPandoc, function (err, stdout, stderr) {
+      console.log(stdout);
+      if ( err ) { console.log(err); } //else prints null
+      console.log(stderr);
+      if ( !err ) { next(); } //heres the callback....
+    });
+  }
 }
 
 //insert values for the next tuple - primary key id in the table auto-increments
 function insertChangelog(date_posted, version, changelog, author_id) {
-	connection.query('INSERT INTO changes (date_posted, title, changes, author_id) values ("'+date_posted+'", "'+version+'", "'+changelog+'", "'+author_id+'")',
-		function (error, results, fields) {
-        if (error) { console.log('ERRORS=', error); }
-        if (results) { console.log('RESULTS=', results); }
-        if (fields) { console.log('FIELDS=', fields); }
-    });
-         
-    connection.query('INSERT INTO news (date_posted, title, news, author_id) values ("'+date_posted+'", "QuickPlay '+version+' is released", "'+changelog+'", "'+author_id+'")',
-		function (error, results, fields) {
-        if (error) { console.log('ERRORS=', error); }
-        if (results) { console.log('RESULTS=', results); }
-        if (fields) { console.log('FIELDS=', fields); }
+  connection.query('INSERT INTO changes (date_posted, title, changes, author_id) values ("'+date_posted+'", "'+version+'", "'+changelog+'", "'+author_id+'")',
+  function (error, results, fields) {
+  if (error) { console.log('ERRORS=', error); }
+  if (results) { console.log('RESULTS=', results); }
+  if (fields) { console.log('FIELDS=', fields); }
+  });
 
+  connection.query('INSERT INTO news (date_posted, title, news, author_id) values ("'+date_posted+'", "QuickPlay '+version+' is released", "'+changelog+'", "'+author_id+'")',
+  function (error, results, fields) {
+    if (error) { console.log('ERRORS=', error); }
+    if (results) { console.log('RESULTS=', results); }
+    if (fields) { console.log('FIELDS=', fields); }
 
-        connection.end(); //todo: move
-    });
-
+    connection.end(); //todo: move
+  });
 }
-
-
 
--- build/updateChangelogAndVersion/updateQPVersion.js
+++ build/updateChangelogAndVersion/updateQPVersion.js
@@ -1,34 +1,33 @@
 /**
  * Reads the local copy of the qp database and updates QPVer
  */
-var fs							=	require("fs"),
-		connection			=	require('./dbConnect'),
-		version_name		=	process.env.version, //quickplay version number we're releasing
-		qpZip						=	process.env.localZipFile,
-		stats						=	fs.statSync(qpZip),
-		zipSizeInBytes	=	stats.size,
-		filesize				=	Math.floor(zipSizeInBytes / 1000000) + "MB",
-		downloadURL			=	process.env.sfFileURL; 
+var fs = require('fs'),
+  connection = require('./dbConnect');
+
+var versionName = process.env.version, //quickplay version number we're releasing
+  qpZip = process.env.localZipFile,
+  stats	= fs.statSync(qpZip),
+  zipSizeInBytes = stats.size,
+  filesize = Math.floor(zipSizeInBytes / 1000000) + 'MB',
+  downloadURL = process.env.sfFileURL;
 
 function updateQPVerManually() { 
-	var prompt				= require('./userInput'),
-			getVersion		= require('./getVersion'),
-			version_name	= getVersion(), //to force return value not function itself
-			downloadURl		= result.downloadURL,
-			filesize			= result.filesize;
-	
-	console.log('Enter the details for Version ' + version_name); //force string return value not function code
-
+  var prompt				= require('./userInput'),
+    getVersion = require('./getVersion'),
+    versionName = getVersion(), //to force return value not function itself
+    downloadURl = result.downloadURL,
+    filesize = result.filesize;
+    console.log('Enter the details for Version ' + versionName); //force string return value not function code
 }
 
 //the callback we pass to connect runs after we connect, or fail to
 connection.connect(function(err) {
   if (err) {
     console.error('error connecting: ' + err.stack);
+   
     return;
   }
   console.log('connected as id ' + connection.threadId);
-
 });
 
 /*
@@ -41,32 +40,30 @@ connection.connect(function(err) {
 //prompt(function(result){ //if we make the manual update we'll need to add this to the flow
 //async-101: we can't call insertRow serially after show table, or it'll run both at the same time
 showTable(function() {
-   insertRow(version_name, downloadURL, filesize);
+   insertRow(versionName, downloadURL, filesize);
 }); //here's why we use promises, this could become the callback pyramid of doom
 
 function showTable(next) {
-    console.log('EXISTING VERSION TABLE FOLLOWS:');
-    connection.query('select * from version', function (error, results, fields) {
-        // error will be an Error if one occurred during the query
-        if (error) { console.log('ERRORS=', error); }
-        // results will contain the results of the query
-        if (results) { console.log('RESULTS=', results); }
-        // fields will contain information about the returned results fields (if any)
-        if (fields) { console.log('FIELDS=', fields); }
-        next(); //call next function async
-    });
+  console.log('EXISTING VERSION TABLE FOLLOWS:');
+  connection.query('select * from version', function (error, results, fields) {
+    // error will be an Error if one occurred during the query
+    if (error) { console.log('ERRORS=', error); }
+    // results will contain the results of the query
+    if (results) { console.log('RESULTS=', results); }
+    // fields will contain information about the returned results fields (if any)
+    if (fields) { console.log('FIELDS=', fields); }
+    next(); //call next function async
+  });
 }
 
 //insert values for the next tuple - primary key version_id in the table auto-increments
-function insertRow(version_name, downloadURL, filesize) {
-    connection.query('INSERT INTO version (version_name, download, filesize) values ("'+version_name+'","'+downloadURL+'", "'+filesize+'")',
-        function (error, results, fields) {
-        if (error) { console.log('ERRORS=', error); }
-        if (results) { console.log('RESULTS=', results); }
-        if (fields) { console.log('FIELDS=', fields); }
-        connection.end(); //todo: move
-    });
-
+function insertRow(versionName, downloadURL, filesize) {
+  connection.query('INSERT INTO version (versionName, download, filesize) values ("'+versionName+'","'+downloadURL+'", "'+filesize+'")',
+  function (error, results, fields) {
+    if (error) { console.log('ERRORS=', error); }
+    if (results) { console.log('RESULTS=', results); }
+    if (fields) { console.log('FIELDS=', fields); }
+    connection.end(); //todo: move
+  });
 }
-
 
